644d9c8d16f97c5b19fc516efd940506670ab8ac
Eli Craig <elidcraig@gmail.com>
2022-06-14 11:13:17 -0700

    commit tree 7feb7e693853c62056c3b0e96ea4b56aa55ffd80

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list
+    self.actors.collect {|actor_obj| actor_obj.full_name}
+  end
 end
---
+++ db/seeds.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+seasons = ['Fall', 'Winter', 'Spring', 'Summer']
+
+puts 'Clearing out old data...'
+
+Actor.destroy_all
+Character.destroy_all
+Network.destroy_all
+Show.destroy_all
+
+puts 'Seeding new data...'
+
+channels = [1, 2, 3, 4, 15, 16, 34, 52]
+channels.each do |channel|
+  Network.create(channel: channel, call_letters: Faker::Alphanumeric.alphanumeric(number: 4, min_alpha: 4).upcase)
+end
+
+30.times do
+  Actor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+
+network_ids = Network.all
+
+10.times do
+  Show.create(name: Faker::Movie.title, day: days.sample, season: seasons.sample, genre: Faker::Book.genre, network: network_ids.sample)
+end
+# actors
+# shows
+# characters
+
+puts 'Data planted!'
